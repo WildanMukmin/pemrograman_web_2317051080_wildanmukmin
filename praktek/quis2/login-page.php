0c147295899ea51587c1347d08bc3db2cf0b9cc2
WildanMukmin <wildanmukmin26@gmail.com>
2025-05-23 11:23:45 +0700

    alert

--- praktek/quis2/login-page.php
+++ praktek/quis2/login-page.php
@@ -25,6 +25,9 @@ if (isset($_SESSION['user'])) {
                         <?php if (isset($_SESSION['error'])): ?>
                             <div class="alert alert-danger"><?= $_SESSION['error']; unset($_SESSION['error']); ?></div>
                         <?php endif; ?>
+                        <?php if (isset($_SESSION['success'])): ?>
+                            <div class="alert alert-success"><?= $_SESSION['success']; unset($_SESSION['success']); ?></div>
+                        <?php endif; ?>
 
                         <form method="POST" action="login.php">
                             <div class="mb-3">
